1a68d6ab16f42f3b990b1f71b7afff2c6b0678cb
Keaton Law <keaton.c.law@gmail.com>
2023-04-10 17:55:22 -0500

    strings and slicin

---
+++ stringbasic.py
@@ -0,0 +1,16 @@
+msg='strangs and stuff'
+#    012345678 ect.
+print(msg)
+#upper/lower will change case of chars, either all upper or all lower.
+print(msg.upper())
+#capitalize will capitalize first char
+print(msg.capitalize())
+#title will capitalize beginning letter of every word
+print(msg.title())
+#len is the length of msg
+print(len(msg))
+#counts the instance of whatever string/char
+print(msg.count('s'))
+#slicing can get pretty wild
+print(msg[-1])
+print(msg[2:7])
